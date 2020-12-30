c0d8672d6a7ad1f45bdb446b9b10ddf004f9a520
Shijing Liu <brendaliu0628@gmail.com>
2020-12-30 15:34:06 -0600

    mail body layout is done

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 2020-12-30
 
-Redux, React Router, firebase auth, react hook form, material UI, css flex.
+Redux, React Router, React Hooks (useHistory to change email list to email detail page) firebase auth, react hook form, material UI, css flex.
 <br>
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app), using the [Redux](https://redux.js.org/) and [Redux Toolkit](https://redux-toolkit.js.org/) template.
--- src/App.js
+++ src/App.js
@@ -2,12 +2,35 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import Sidebar from './components/Sidebar/Sidebar';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Mail from './components/Mail/Mail';
+import EmailList from './components/EmailList/EmailList';
+
+
+
+
 
 function App() {
   return (
     <div className="app">
       <Header />
-      <Sidebar />
+
+      <div className="app__body">
+        <Sidebar />
+        <Switch>
+          <Route path="/mail">
+            <Mail />
+          </Route>
+          <Route path="/">
+            <EmailList />
+          </Route>
+        </Switch>
+      </div>
     </div>
   );
 }
